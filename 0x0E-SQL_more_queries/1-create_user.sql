7ebb60cec7efbff6e81b39f660d30148b1f1e200
NwangwuChidera <nwangwupamela@gmail.com>
2023-02-15 11:34:43 -0800

    tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create a user user_0d_1
+-- The user password should be set to user_0d_1_pwd
+-- user_0d_1 should have all privileges on your MySQL server
+-- If the user user_0d_1 already exists, your script should not fail.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1'@'localhost
